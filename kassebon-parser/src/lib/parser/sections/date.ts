1304704c87ffc2a9b1fcc56458af060bf9d5513c
Rodrigo Duarte <rodrigo.terminus@gmail.com>
2021-04-10 14:45:11 +0200

    Fix date parsing

--- kassebon-parser/src/lib/parser/sections/date.ts
+++ kassebon-parser/src/lib/parser/sections/date.ts
@@ -4,14 +4,8 @@ export default class DateParser implements Parser<Date> {
   parse(content: string): Date {
     const { date: dateString } =
       content.match(/Datum:\s+(?<date>[\d\.]+)/)?.groups || {}
-    const { time: timeString } =
-      content.match(/Uhrzeit:\s+(?<time>[\d:]+)/)?.groups || {}
-    const datePieces = dateString.split('.')
-    const timePieces = timeString.split(':')
-    const dateArgs = [...datePieces.reverse(), ...timePieces].map((s) =>
-      parseInt(s, 10),
-    ) as []
-
-    return new Date(...dateArgs)
+    const { time } = content.match(/Uhrzeit:\s+(?<time>[\d:]+)/)?.groups || {}
+    const date = dateString.split('.').reverse().join('-')
+    return new Date(`${date}T${time}`)
   }
 }
